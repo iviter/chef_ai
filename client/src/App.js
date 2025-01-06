bf17930516ad2c3661332a7c0217a05ae6a0b00d
Ivan Viter <ivanviter@i.ua>
2025-01-06 20:10:09 +0100

    Create basic recipes UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Recipies from './components/recipes';
+import { useEffect, useState } from 'react';
 
-function App() {
+const API_URL = 'http://localhost:3000/api/v1/recipes';
+
+const getApiData = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    const fetchData = async () => {
+      const data = await getApiData();
+      if (mounted) {
+        setRecipes(data);
+      }
+    };
+
+    fetchData();
+
+    return () => { (mounted = false) };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {recipes.length > 0 && <Recipies recipes={recipes}/> }
     </div>
   );
-}
+};
 
 export default App;
