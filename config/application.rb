1f6babc8a8a4f1bce4fbca524e0fe9abfeb114b4
Ivan Viter <ivanviter@i.ua>
2025-01-06 15:48:21 +0100

    Remove system test run from CI config

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,5 @@ module RecipeApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.generators.system_tests = nil
   end
 end
