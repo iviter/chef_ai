58f380569eb68bf1d2b7c8c54548dd3ca821ad4e
Ivan Viter <ivanviter@i.ua>
2025-01-07 21:44:22 +0100

    Add search endpoint to RecipesController

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,19 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[ show update destroy ]
-
-  # GET /recipes
   def index
     @recipes = Recipe.all
 
     render json: @recipes
   end
 
-  # GET /recipes/1
-  def show
-    render json: @recipe
-  end
-
-  # POST /recipes
   def create
     @recipe = Recipe.new(recipe_params)
 
@@ -24,29 +15,17 @@ class Api::V1::RecipesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /recipes/1
-  def update
-    if @recipe.update(recipe_params)
-      render json: @recipe
-    else
-      render json: @recipe.errors, status: :unprocessable_entity
-    end
-  end
+  def search
+    response = GroqAiClientResponse.new(recipe_params.to_h).call
 
-  # DELETE /recipes/1
-  def destroy
-    @recipe.destroy!
+    render json: [ response.message ]
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
 
-  def set_recipe
-    @recipe = Recipe.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def recipe_params
-    params.require(:recipe).permit(:name, :description)
+    params.require(:recipe).permit(:description, :name, :dishType, :cuisine, :mainIngredients, :dietaryRestrictions,
+                                   :allergies, :servings, :cookingTime, :cookingMethod, :flavorProfile,
+                                   :specialOccasion)
   end
 end
