ba19894dd05bbd59f0a38e38a99b9420c45e4d02
Ivan Viter <ivanviter@i.ua>
2025-01-07 21:45:16 +0100

    Require groq_ai_response_decorator class in config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
+require './app/decorators/groq_ai_response_decorator'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
