22d8db24eb5ba908234200185371adaca8115056
Ivan Viter <ivanviter@i.ua>
2025-01-08 16:58:21 +0100

    Refactor App component to extract API call inside parent component

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,40 @@
 import './App.css';
 import axios from 'axios';
-import Recipies from './components/recipes';
-import { useEffect, useState } from 'react';
-
-const API_URL = 'http://localhost:3000/api/v1/recipes';
-
-const getApiData = async () => {
-  try {
-    const response = await axios.get(API_URL);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
+import RecipeList from './components/RecipeGenerator/RecipeList';
+import { useState } from 'react';
+import RecipeForm from './components/RecipeGenerator/RecipeForm';
 
 const App = () => {
-  const [recipes, setRecipes] = useState([]);
+  const [error, setError] = useState(null); 
+  const [loading, setLoading] = useState(false);
+  const [recipe, setRecipe] = useState(null); 
 
-  useEffect(() => {
-    let mounted = true;
-    const fetchData = async () => {
-      const data = await getApiData();
-      if (mounted) {
-        setRecipes(data);
-      }
-    };
+  const RECIPE_BASE_URL = process.env.REACT_APP_RECIPE_BASE_URL;
+  const DEFAULT_ERROR_MESSAGE = 'Failed to generate valid recipe. Please select appropriate ingredients and try again!'
 
-    fetchData();
+  const generateRecipe = async (data) => {
+    setLoading(true);
+    try {
+      const response = await axios.post(`${RECIPE_BASE_URL}/recipes/`, { recipe: data });
+      setRecipe(response.data);
+      setError(null);
+    } catch (error) {
+      setError(DEFAULT_ERROR_MESSAGE); 
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return () => { (mounted = false) };
-  }, []);
+  {loading && <p>Loading...</p>}
+  {error && <p className="error">{error}</p>} 
 
   return (
     <div className="App">
-      {recipes.length > 0 && <Recipies recipes={recipes}/> }
+      <h1>Generate your recipe here:</h1>
+      <RecipeForm generateRecipe={generateRecipe} />
+      {loading && <p>Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {!loading && <RecipeList recipe={recipe} /> }
     </div>
   );
 };
