f021e1831181e1a53d146951637746d513c4328c
Ivan Viter <ivanviter@i.ua>
2025-01-08 14:18:17 +0100

    Create RecipeGeneratorService

---
+++ app/services/recipe_generator_service.rb
@@ -0,0 +1,23 @@
+class RecipeGeneratorService
+  DEFAULT_MESSAGE = { 'message' => 'Please provide step-by-step recipe for a dish based on the provided ingredients' }
+
+  def initialize(recipe_params)
+    @recipe_params = filter(recipe_params)
+  end
+
+  def call
+    GroqAiClientResponse.new(recipe_params).call
+  end
+
+  private
+
+  attr_reader :recipe_params
+
+  def filter(params)
+    params
+      .reject { |_, v| v.blank? }
+      .merge(DEFAULT_MESSAGE)
+      .map { |k, v| "#{k}: #{v}" }
+      .join(', ')
+  end
+end
