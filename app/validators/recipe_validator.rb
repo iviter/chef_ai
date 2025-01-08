f0abdfd36a12483970e278a7413bcefd377e02b7
Ivan Viter <ivanviter@i.ua>
2025-01-08 14:18:31 +0100

    Create RecipeValidator

---
+++ app/validators/recipe_validator.rb
@@ -0,0 +1,19 @@
+class RecipeValidator
+  def initialize(recipe)
+    @recipe = recipe
+  end
+
+  def valid_recipe?
+    response = GroqAiClientResponse.new(prompt).call
+    response.to_lowercase == 'true'
+  end
+
+  private
+
+  attr_reader :recipe
+
+  def prompt
+    "Please validate the following text and confirm whether it is a valid step-by-step recipe: #{recipe}
+    Respond with true if it is a valid recipe, or false otherwise"
+  end
+end
