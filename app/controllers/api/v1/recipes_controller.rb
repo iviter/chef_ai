aad3987888d184f54c4158cde04aeab1fcf8bc4f
Ivan Viter <ivanviter@i.ua>
2025-01-09 13:25:46 +0100

    Refactor Api::V1::RecipesController logic

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,11 +1,5 @@
 class Api::V1::RecipesController < ApplicationController
-  def index
-    @recipes = Recipe.all
-
-    render json: @recipes
-  end
-
-  def create
+  def search
     recipe = ::RecipeGeneratorService.new(recipe_params.to_h).call
 
     if ::RecipeValidator.new(recipe.message).valid_recipe?
@@ -19,8 +13,7 @@ class Api::V1::RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:description, :name, :dishType, :cuisine, :mainIngredients, :dietaryRestrictions,
-                                   :allergies, :servings, :cookingTime, :cookingMethod, :flavorProfile,
-                                   :specialOccasion)
+    params.require(:recipe).permit(:dishType, :cuisine, :mainIngredients, :dietaryRestrictions, :specialOccasion,
+                                   :allergies, :servings, :cookingTime, :cookingMethod, :flavorProfile)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :recipes, only: %i[index create]
+      post 'recipes/search', to: 'recipes#search'
     end
   end
 end
