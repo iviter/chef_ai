6b4531b65643665f10a6724aabe51159b8e09753
Ivan Viter <ivanviter@i.ua>
2025-01-09 20:33:02 +0100

    Add request specs

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,63 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe "/recipes", type: :request do
-  # This should return the minimal set of attributes required to create a valid
-  # Recipe. As you add validations to Recipe, be sure to
-  # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  # This should return the minimal set of values that should be in the headers
-  # in order to pass any filters (e.g. authentication) defined in
-  # RecipesController, or in your router and rack
-  # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
-    {}
-  }
-
-  describe "GET /index" do
-    xit "renders a successful response" do
-      Recipe.create! valid_attributes
-      get recipes_url, headers: valid_headers, as: :json
-      expect(response).to be_successful
-    end
+RSpec.describe '/recipes', type: :request do
+  subject(:api_request) do
+    post api_v1_recipes_search_path, params: params, headers: headers, as: :json
   end
 
-  describe "POST /create" do
-    xcontext "with valid parameters" do
-      it "creates a new Recipe" do
-        expect {
-          post recipes_url,
-               params: { recipe: valid_attributes }, headers: valid_headers, as: :json
-        }.to change(Recipe, :count).by(1)
+  let(:valid_attributes) do
+    {
+      dishType: 'Main Course',
+      cuisine: 'Italian',
+      mainIngredients: [ 'Pasta', 'Tomato' ],
+      dietaryRestrictions: 'None',
+      specialOccasion: 'None',
+      allergies: 'None'
+    }
+  end
+
+  let(:invalid_attributes) { { dishType: nil, cuisine: nil, mainIngredients: [] } }
+  let(:valid_headers) { {} }
+
+  describe 'POST /search' do
+    context 'with valid parameters' do
+      let(:params) { { recipe: valid_attributes  } }
+      let(:headers) { valid_headers }
+
+      before do
+        VCR.use_cassette('groq_api_search_success') do
+          api_request
+        end
       end
 
-      it "renders a JSON response with the new recipe" do
-        post recipes_url,
-             params: { recipe: valid_attributes }, headers: valid_headers, as: :json
-        expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+      it 'returns a successful response with recipe data' do
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to match(a_string_including('application/json'))
+        expect(response.body).to include('recipe')
       end
     end
 
-    xcontext "with invalid parameters" do
-      it "does not create a new Recipe" do
-        expect {
-          post recipes_url,
-               params: { recipe: invalid_attributes }, as: :json
-        }.to change(Recipe, :count).by(0)
+    context 'with invalid parameters' do
+      let(:params) { { recipe: invalid_attributes  } }
+      let(:headers) { valid_headers }
+      let(:error_message) { JSON.parse(response.body) }
+
+      before do
+        VCR.use_cassette('groq_api_search_failed') do
+          api_request
+        end
       end
 
-      it "renders a JSON response with errors for the new recipe" do
-        post recipes_url,
-             params: { recipe: invalid_attributes }, headers: valid_headers, as: :json
+      it 'renders correct error response' do
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
+        expect(error_message['error']).to eq('Failed to generate a valid recipe. Please select appropriate '\
+          'ingredients and try again.')
       end
     end
   end
