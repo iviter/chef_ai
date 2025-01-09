44a8f4345d6cc15c05d52440049971ed8106075f
Ivan Viter <ivanviter@i.ua>
2025-01-09 11:43:09 +0100

    Cleanup App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,25 @@
 import './App.css';
-import axios from 'axios';
 import RecipeList from './components/RecipeGenerator/RecipeList';
 import { useState } from 'react';
 import RecipeForm from './components/RecipeGenerator/RecipeForm';
+import generateRecipe from './components/api/apiService'
 
 const App = () => {
   const [error, setError] = useState(null); 
   const [loading, setLoading] = useState(false);
-  const [recipe, setRecipe] = useState(null); 
+  const [recipe, setRecipe] = useState(null);
 
-  const RECIPE_BASE_URL = process.env.REACT_APP_RECIPE_BASE_URL;
-  const DEFAULT_ERROR_MESSAGE = 'Failed to generate valid recipe. Please select appropriate ingredients and try again!'
-
-  const generateRecipe = async (data) => {
-    setLoading(true);
-    try {
-      const response = await axios.post(`${RECIPE_BASE_URL}/recipes/`, { recipe: data });
-      setRecipe(response.data);
-      setError(null);
-    } catch (error) {
-      setError(DEFAULT_ERROR_MESSAGE); 
-    } finally {
-      setLoading(false);
-    }
+  const handleGenerateRecipe = async (recipeData) => {
+    await generateRecipe(recipeData, setRecipe, setLoading, setError); 
   };
-
-  {loading && <p>Loading...</p>}
-  {error && <p className="error">{error}</p>} 
 
   return (
     <div className="App">
       <h1>Generate your recipe here:</h1>
-      <RecipeForm generateRecipe={generateRecipe} />
+      <RecipeForm generateRecipe={handleGenerateRecipe} />
       {loading && <p>Loading...</p>}
       {error && <p className="error">{error}</p>}
-      {!loading && <RecipeList recipe={recipe} /> }
+      {!loading && <RecipeList recipeText={recipe} /> }
     </div>
   );
 };
