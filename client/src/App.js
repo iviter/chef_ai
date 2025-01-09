8669343133850ddf745d36d49350b621d22379c3
Ivan Viter <ivanviter@i.ua>
2025-01-09 12:25:48 +0100

    Style App component with tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
 import RecipeList from './components/RecipeGenerator/RecipeList';
 import { useState } from 'react';
 import RecipeForm from './components/RecipeGenerator/RecipeForm';
-import generateRecipe from './components/api/apiService'
+import generateRecipe from './components/api/apiService';
 
 const App = () => {
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [recipe, setRecipe] = useState(null);
 
   const handleGenerateRecipe = async (recipeData) => {
-    await generateRecipe(recipeData, setRecipe, setLoading, setError); 
+    await generateRecipe(recipeData, setRecipe, setLoading, setError);
   };
 
   return (
-    <div className="App">
-      <h1>Generate your recipe here:</h1>
-      <RecipeForm generateRecipe={handleGenerateRecipe} />
-      {loading && <p>Loading...</p>}
-      {error && <p className="error">{error}</p>}
-      {!loading && <RecipeList recipeText={recipe} /> }
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center py-1 px-4">
+      <h1 className="text-3xl font-bold text-blue-600 mb-1">Generate Your Recipe:</h1>
+      <div className="w-full max-w-2xl bg-white shadow-lg rounded-lg p-6">
+        <RecipeForm generateRecipe={handleGenerateRecipe} />
+        {loading && <p className="text-center text-blue-500 font-medium mt-4">Loading...</p>}
+        {error && <p className="text-center text-red-500 font-medium mt-4">{error}</p>}
+        {!loading && <RecipeList recipeText={recipe} />}
+      </div>
     </div>
   );
 };
