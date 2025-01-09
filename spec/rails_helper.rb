061e98396bea56804209ffd50b75c113b4c7103c
Ivan Viter <ivanviter@i.ua>
2025-01-09 20:34:27 +0100

    Add VCR config to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'rubygems'
+require 'vcr'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 # Uncomment the line below in case you have `--require rails_helper` in the `.rspec` file
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
@@ -67,4 +70,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  VCR.configure do |config|
+    config.cassette_library_dir = "#{::Rails.root}/spec/fixtures/vcr_cassettes"
+    config.hook_into :webmock
+    config.allow_http_connections_when_no_cassette = false
+  end
 end
